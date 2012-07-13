d5ed0a765fffa47ad2c3dfd8db07ae0c1026db9f
André Grötschel <a.groetschel@qayqay.de>
2012-07-13 11:20:41 +0200

    add commandlinefu.com functions

--- zshlib/zshhelper.zsh
+++ zshlib/zshhelper.zsh
@@ -206,3 +206,78 @@ function homeclean() {
     echo "no url"
   fi
   }
+
+
+# Escape potential tarbombs
+# http://www.commandlinefu.com/commands/view/6824/escape-potential-tarbombs
+etb() {
+  l=$(tar tf $1);
+  if [ $(echo "$l" | wc -l) -eq $(echo "$l" | grep $(echo "$l" | head -n1) | wc -l) ];
+  then tar xf $1;
+  else mkdir ${1%.t(ar.gz||ar.bz2||gz||bz||ar)} && tar xvf $1 -C ${1%.t(ar.gz||ar.bz2||gz||bz||ar)};
+  fi ;
+}
+
+# show newest files
+# http://www.commandlinefu.com/commands/view/9015/find-the-most-recently-changed-files-recursively
+newest () {find . -type f -printf '%TY-%Tm-%Td %TT %p\n' | grep -v cache | grep -v ".hg" | grep -v ".git" | sort -r | less }
+
+# http://www.commandlinefu.com/commands/view/7294/backup-a-file-with-a-date-time-stamp
+buf () {
+    oldname=$1;
+    if [ "$oldname" != "" ]; then
+        datepart=$(date +%Y-%m-%d);
+        firstpart=`echo $oldname | cut -d "." -f 1`;
+        newname=`echo $oldname | sed s/$firstpart/$firstpart.$datepart/`;
+        cp -R ${oldname} ${newname};
+    fi
+}
+
+function printHookFunctions () {
+    print -C 1 ":::pwd_functions:" $chpwd_functions
+    print -C 1 ":::periodic_functions:" $periodic_functions
+    print -C 1 ":::precmd_functions:" $precmd_functions
+    print -C 1 ":::preexec_functions:" $preexec_functions
+    print -C 1 ":::zshaddhistory_functions:" $zshaddhistory_functions
+    print -C 1 ":::zshexit_functions:" $zshexit_functions
+}
+
+# Rename files in a directory in an edited list fashion
+# http://www.commandlinefu.com/commands/view/7818/
+function massmove () {
+    ls > ls; paste ls ls > ren; vi ren; sed 's/^/mv /' ren|bash; rm ren ls
+}
+
+
+# Put a console clock in top right corner
+# http://www.commandlinefu.com/commands/view/7916/
+function clock () {
+    while sleep 1;
+    do
+        tput sc
+        tput cup 0 $(($(tput cols)-29))
+        date
+        tput rc
+    done &
+}
+
+# create a new script, automatically populating the shebang line, editing the
+# script, and making it executable.
+# http://www.commandlinefu.com/commands/view/8050/
+shebang() {
+    if i=$(which $1);
+    then
+        printf '#!/usr/bin/env %s\n\n' $1 > $2 && chmod 755 $2 && vim + $2 && chmod 755 $2;
+    else
+        echo "'which' could not find $1, is it in your \$PATH?";
+    fi;
+    # in case the new script is in path, this throw out the command hash table and
+    # start over  (man zshbuiltins)
+    rehash
+}
+
+# translate via google language tools (more lightweight than leo)
+# http://www.commandlinefu.com/commands/view/5034/
+translate() {
+    wget -qO- "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=$1&langpair=$2|${3:-en}" | sed 's/.*"translatedText":"\([^"]*\)".*}/\1\n/'
+}
